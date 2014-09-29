c4c269296dda2c0f4fd7855f3e5dd04dcd857538
soneboy <nebojsamasic2@gmail.com>
2014-09-29 13:47:42 +0200

    Update conf.php

--- conf.php
+++ conf.php
@@ -1,7 +1,7 @@
 <?php 
-defined('DB_SERVER') ? null : define("DB_SERVER", "localhost");
-defined('DB_USER')   ? null : define("DB_USER", "soneboy");
-defined('DB_PASS')   ? null : define("DB_PASS", "84sonatan");
-defined('DB_NAME')   ? null : define("DB_NAME", "tutorials");
+defined('DB_SERVER') ? null : define("DB_SERVER","");
+defined('DB_USER')   ? null : define("DB_USER", "");
+defined('DB_PASS')   ? null : define("DB_PASS", "");
+defined('DB_NAME')   ? null : define("DB_NAME", "");
 
 ?>
